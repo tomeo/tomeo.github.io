a7020cab8d088440d91eab6ba3359f9cbcc560b4
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2024-05-28 00:08:59 +0200

    Add symbols

---
+++ symbols/data.js
@@ -0,0 +1,216 @@
+
+const symbols = [
+    {
+        "value": "a",
+        "answers": [
+            "Albert",
+            "Anna",
+        ],
+    },
+    {
+        "value": "b",
+        "answers": [
+            "Butterfly",
+            "Banana",
+        ],
+    },
+    {
+        "value": "c",
+        "answers": [
+            "Car",
+            "Cinema",
+            "Comet",
+        ],
+    },
+    {
+        "value": "d",
+        "answers": [
+            "Daddy",
+            "Daddy Pig",
+        ],
+    },
+    {
+        "value": "e",
+        "answers": [
+            "Ellie",
+            "Emma",
+            "Erik",
+            "Ella",
+            "Elton",
+        ],
+    },
+    {
+        "value": "f",
+        "answers": [
+            "Family",
+            "Fredric",
+            "Frank",
+        ],
+    },
+    {
+        "value": "g",
+        "answers": [
+            "Granddad",
+            "Granny Lynda",
+            "George",
+        ],
+    },
+    {
+        "value": "h",
+        "answers": [
+            "Hug",
+            "Happy",
+        ],
+    },
+    {
+        "value": "i",
+        "answers": [
+            "Infinity",
+        ],
+    },
+    {
+        "value": "j",
+        "answers": [
+            "Jumping",
+            "Jonathan",
+        ],
+    },
+    {
+        "value": "k",
+        "answers": [
+            "Kids",
+            "Keyboard",
+            "Kiss",
+        ],
+    },
+    {
+        "value": "l",
+        "answers": [
+            "Louie",
+            "Lena",
+        ],
+    },
+    {
+        "value": "m",
+        "answers": [
+            "Mammy",
+            "Mummy Pig",
+            "Mormor",
+            "Maj",
+            "Marie",
+        ],
+    },
+    {
+        "value": "n",
+        "answers": [
+            "Nanna",
+        ],
+    },
+    {
+        "value": "p",
+        "answers": [
+            "Pappa",
+            "Planet",
+            "Peppa Pig",
+        ],
+    },
+    {
+        "value": "r",
+        "answers": [
+            "Reading",
+            "Rain",
+        ],
+    },
+    {
+        "value": "s",
+        "answers": [
+            "Swimming",
+            "Swinging",
+            "Sister",
+            "Sad",
+        ],
+    },
+    {
+        "value": "t",
+        "answers": [
+            "Tim",
+            "Tesla",
+        ],
+    },
+    {
+        "value": "u",
+        "answers": [
+            "Uncle David",
+        ],
+    },
+    {
+        "value": "w",
+        "answers": [
+            "Walking",
+        ],
+    },
+    {
+        "value": "y",
+        "answers": [
+            "Yellow Submarine",
+        ],
+    },
+    {
+        "value": "1",
+        "answers": [
+            "Dinosaur",
+        ],
+    },
+    {
+        "value": "2",
+        "answers": [
+            "Sandcastles",
+        ],
+    },
+    {
+        "value": "3",
+        "answers": [
+            "Candles",
+        ],
+    },
+    {
+        "value": "4",
+        "answers": [
+            "Bicycles",
+        ],
+    },
+    {
+        "value": "5",
+        "answers": [
+            "Friends",
+        ],
+    },
+    {
+        "value": "6",
+        "answers": [
+            "Ducks",
+        ],
+    },
+    {
+        "value": "7",
+        "answers": [
+            "Toys",
+        ],
+    },
+    {
+        "value": "8",
+        "answers": [
+            "Boots",
+        ],
+    },
+    {
+        "value": "9",
+        "answers": [
+            "Flowers",
+        ],
+    },
+    // {
+    //     "value": "10",
+    //     "word": "Balloons",
+    // },
+];
---
+++ symbols/script.js
@@ -0,0 +1,86 @@
+function preloadVoices() {
+    return new Promise((resolve) => {
+        let voices = window.speechSynthesis.getVoices();
+        if (voices.length) {
+        resolve(voices);
+        } else {
+        window.speechSynthesis.onvoiceschanged = () => {
+            voices = window.speechSynthesis.getVoices();
+            resolve(voices);
+        };
+        }
+    });
+}
+
+function speak(message, callback) {
+    const voices = speechSynthesis.getVoices();
+    const selectedVoice = voices.find(voice => voice.name === 'Google UK English Female');
+    const utterance = new SpeechSynthesisUtterance(message);
+    utterance.onend = callback;
+    utterance.voice = selectedVoice;
+    window.speechSynthesis.speak(utterance);
+}
+
+function getRandom(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function randomBool() {
+    return Math.random() < 0.5;
+}
+
+function changeSymbol(symbol) {
+    document.getElementById('symbol').innerText = symbol.value;
+}
+
+function countingPhrase(value, answer) {
+    let max = Number(value);
+    let numbers = [];
+    for(var i = 1; i <= max; i++) numbers.push(i);
+    let numbersStr = numbers.join(", ");
+    return `${numbersStr}. ${value} ${answer}`
+}
+
+function phrase(symbol, answer) {
+    return isNaN(symbol.value)
+    ? `${symbol.value.toUpperCase()} is for ${answer}`
+    : countingPhrase(symbol.value, answer);
+}
+
+function changeText(symbol, answer) {
+    document.getElementById('text').innerText = phrase(symbol, answer);
+    document.getElementById('text').classList.remove("hidden");
+}
+
+function changeImage(answer) {
+    document.getElementById('img').src = `${answer.toLowerCase()}.jpg`;
+    document.getElementById('img').classList.remove("hidden");
+}
+
+function reset(symbol) {
+    changeSymbol(symbol);
+    document.getElementById('img').classList.add("hidden");
+    document.getElementById('text').classList.add("hidden");
+}
+
+function correct(symbol, answer) {
+    changeText(symbol, answer);
+    changeImage(answer);
+    speak(`Yes! That is ${symbol.value}. ${phrase(symbol, answer)}`, function () {
+        reset(getRandom(symbols));
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    preloadVoices().then(() => {
+        reset(getRandom(symbols));
+        document.addEventListener('keydown', function(event) {
+            let currentValue = document.getElementById('symbol').innerText.toLowerCase()
+            let currentSymbol = symbols.find(s => s.value.toLowerCase() == currentValue);
+            if (event.key === currentSymbol.value.toLowerCase()) {
+                let answer = getRandom(currentSymbol.answers);
+                correct(currentSymbol, answer);
+            }
+        });
+      });
+});
