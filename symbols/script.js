b1be4a046c827fb48bdce72df7d546d8349842cd
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2024-05-29 22:54:55 +0200

    Remove fireworks

--- symbols/script.js
+++ symbols/script.js
@@ -77,20 +77,21 @@ function symbol_type(value) {
     return isNaN(value) ? "letter" : "number";
 }
 
-function correct(symbol, answer, fireworks) {
-    fireworks.start();
+// function correct(symbol, answer, fireworks) {
+    function correct(symbol, answer) {
+    // fireworks.start();
     document.getElementById('symbol').classList.add("hidden");
     document.getElementById('symbol').value = "";
     changeText(symbol, answer);
     changeImage(answer);
     speak(`Yes! That is the ${symbol_type(symbol.value)} ${symbol.value}. ${phrase(symbol, answer)}`, function () {
         reset(getRandom(symbols));
-        fireworks.stop();
+        // fireworks.stop();
     });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    const fireworks = new Fireworks.default(document.querySelector("#main"));
+    // const fireworks = new Fireworks.default(document.querySelector("#main"));
 
     document.getElementById('symbol').addEventListener('keypress', function(event) {
         event.preventDefault();
@@ -103,7 +104,8 @@ document.addEventListener('DOMContentLoaded', function() {
             let currentSymbol = symbols.find(s => s.value.toLowerCase() == currentValue);
             if (event.key === currentSymbol.value.toLowerCase()) {
                 let answer = getRandom(currentSymbol.answers);
-                correct(currentSymbol, answer, fireworks);
+                // correct(currentSymbol, answer, fireworks);
+                correct(currentSymbol, answer);
             }
         });
       });
